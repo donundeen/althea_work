25fb4b7341b9a08cb27da1d2ba49de4ac1ca0408
don undeen <donundeen@gmail.com>
2019-05-06 20:08:11 -0400

    new files

--- tests/background_detection_bgs.py
+++ tests/background_detection_bgs.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import bgs
 import time
 from picamera.array import PiRGBArray
 from picamera import PiCamera
@@ -15,6 +16,16 @@ camera.resolution = (640, 480)
 #camera.resolution = (64, 64)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+
+#better ones
+#algorithm = bgs.AdaptiveBackgroundLearning()
+#algorithm = bgs.FrameDifference()
+#algorithm = bgs.StaticFrameDifference()
+
+#algorithm = bgs.AdaptiveSelectiveBackgroundLearning()
+#algorithm = bgs.LOBSTER()
+#algorithm = bgs.DPEigenbackground()
+
  
 # allow the camera to warmupq
 time.sleep(0.1)
@@ -26,8 +37,40 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
-    fgmask = fgbg.apply(image)
+    fgmask = algorithm.apply(image)
     fgmask = np.fliplr(fgmask)
+
+
+    # Threshold.
+    # Set values equal to or above 220 to 0.
+    # Set values below 220 to 255.
+     
+    th, im_th = cv2.threshold(fgmask, 220, 255, cv2.THRESH_BINARY_INV);
+     
+    # Copy the thresholded image.
+    im_floodfill = im_th.copy()
+     
+    # Mask used to flood filling.
+    # Notice the size needs to be 2 pixels than the image.
+    h, w = im_th.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+     
+    # Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);
+     
+    # Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+     
+    # Combine the two images to get the foreground.
+    im_out = im_th | im_floodfill_inv
+
+
+    #fgmask = im_out  
+
+
+
+
+    
 
     fgmask_crop = fgmask[0:480 , 80:560]
 
--- tests/background_detection_fill.py
+++ tests/background_detection_fill.py
@@ -27,13 +27,40 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # and occupied/unoccupied text
     image = frame.array
     fgmask = fgbg.apply(image)
+
+
+    # Threshold.
+    # Set values equal to or above 220 to 0.
+    # Set values below 220 to 255.
+     
+    th, im_th = cv2.threshold(fgmask, 220, 255, cv2.THRESH_BINARY_INV);
+     
+    # Copy the thresholded image.
+    im_floodfill = im_th.copy()
+     
+    # Mask used to flood filling.
+    # Notice the size needs to be 2 pixels than the image.
+    h, w = im_th.shape[:2]
+    mask = np.zeros((h+2, w+2), np.uint8)
+     
+    # Floodfill from point (0, 0)
+    cv2.floodFill(im_floodfill, mask, (0,0), 255);
+     
+    # Invert floodfilled image
+    im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+     
+    # Combine the two images to get the foreground.
+    im_out = im_th | im_floodfill_inv
+
+
+    fgmask = im_floodfill_inv   
     fgmask = np.fliplr(fgmask)
 
     fgmask_crop = fgmask[0:480 , 80:560]
 
     small = cv2.resize(fgmask_crop, (0,0), fx=0.133, fy=0.133) 
 
-    cv2.imshow('MOG',fgmask_crop)
+    cv2.imshow('MOG',small)
     # show the frame
     #cv2.imshow("Frame2", image)
     key = cv2.waitKey(1) & 0xFF
